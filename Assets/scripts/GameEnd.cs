ec48d3e559de8d5bd4595cca6febeca4a2d2be2a
Agustin Pontura <pontura@gmail.com>
2025-08-27 15:46:16 -0300

    Update GameEnd.cs

--- Assets/scripts/GameEnd.cs
+++ Assets/scripts/GameEnd.cs
@@ -1,4 +1,6 @@
 
+using UnityEngine;
+
 public class GameEnd : Gameplay
 {
     public TMPro.TMP_Text[] titles;
@@ -9,9 +11,16 @@ public class GameEnd : Gameplay
         foreach (TMPro.TMP_Text t in titles)
         {
             if (winner == playerID)
+            {
+
                 t.text = GameManager.Instance.settings.gameEnd_win;
+                GetComponent<Animator>().Play("win");
+            }
             else
+            {
                 t.text = GameManager.Instance.settings.gameEnd_lose;
+                GetComponent<Animator>().Play("lose");
+            }
         }
         Invoke("Done", GameManager.Instance.settings.timeForSummary);
     }
